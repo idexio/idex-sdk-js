0eb6701fb9176fe406565a3b4a6e7b1f49cccbca
dileepfrog <dileepfrog@gmail.com>
2020-02-13 13:53:43 -0800

    add sample contract

---
+++ contracts/SignatureVerifier.sol
@@ -0,0 +1,88 @@
+pragma solidity ^0.6.1;
+pragma experimental ABIEncoderV2;
+
+import {ECRecovery} from './ECRecovery.sol';
+
+library SignatureVerifier {
+  enum OrderSide {Buy, Sell}
+  enum OrderType {Limit, Market, StopLimit}
+
+  struct Order {
+    uint128 orderId;
+    OrderSide side;
+    OrderType orderType;
+    address walletAddress;
+    address baseAssetAddress;
+    address quoteAssetAddress;
+    uint128 nonce;
+    uint64 totalQuantity; // pips
+    uint64 limitPrice; // decimal pips * 10^8
+    uint64 stopPrice; // decimal pips * 10^8
+  }
+
+  function validateOrderSignature(
+    Order memory order,
+    string memory marketSymbol,
+    bytes memory signature
+  ) public pure {
+    require(
+      ECRecovery.isSignatureValid(
+        getOrderWalletHash(order, marketSymbol),
+        signature,
+        order.walletAddress
+      ),
+      'Invalid wallet signature for buy order'
+    );
+  }
+
+  function getOrderWalletHash(Order memory order, string memory orderMarketSymbol)
+    private
+    pure
+    returns (bytes32)
+  {
+    return
+      keccak256(
+        abi.encodePacked(
+          abi.encodePacked(
+            orderMarketSymbol,
+            uint8(order.side),
+            uint8(order.orderType),
+            order.baseAssetAddress,
+            order.quoteAssetAddress
+          ),
+          abi.encodePacked(
+            // Ledger qtys and prices are in pip, but order was signed by wallet owner with decimal values
+            pipToDecimal(order.totalQuantity),
+            order.limitPrice > 0 ? pipToDecimal(order.limitPrice) : '',
+            order.stopPrice > 0 ? pipToDecimal(order.stopPrice) : '',
+            order.walletAddress,
+            order.nonce
+          )
+        )
+      );
+  }
+
+  // Inspired by https://github.com/provable-things/ethereum-api/blob/831f4123816f7a3e57ebea171a3cdcf3b528e475/oraclizeAPI_0.5.sol#L1045-L1062
+  function pipToDecimal(uint256 pips) private pure returns (string memory) {
+    uint256 copy = pips;
+    uint256 length;
+    while (copy != 0) {
+      length++;
+      copy /= 10;
+    }
+    if (length < 9) {
+      length = 9; // a zero before the decimal point plus 8 decimals
+    }
+    length++; // for the decimal point
+    bytes memory decimal = new bytes(length);
+    for (uint256 i = length; i > 0; i--) {
+      if (length - i == 8) {
+        decimal[i - 1] = bytes1(uint8(46)); // period
+      } else {
+        decimal[i - 1] = bytes1(uint8(48 + (pips % 10)));
+        pips /= 10;
+      }
+    }
+    return string(decimal);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -5,62 +5,57 @@ import * as models from './models';
 import RequestBuilder from './request-builder';
 import RequestVerifier from './request-verifier';
 
+const limitOrder: models.LimitOrder = {
+  market: 'IDEX-ETH',
+  side: 'buy',
+  type: 'limit',
+  quantity: '1.20000000',
+  price: '0.50000000',
+  // customClientOrderId: '6f392746-4dd9-11ea-ba35-05698b78935d'
+};
+
+const marketOrder: models.MarketOrder = {
+  market: 'IDEX-ETH',
+  side: 'sell',
+  type: 'market',
+  quantity: '1.20000000',
+  // customClientOrderId: '6f392747-4dd9-11ea-ba35-05698b78935d'
+};
+
+const stopLimitOrder: models.StopLimitOrder = {
+  market: 'IDEX-ETH',
+  side: 'buy',
+  type: 'stopLimit',
+  quantity: '1.20000000',
+  price: '0.50000000',
+  stopPrice: '0.60000000',
+  // customClientOrderId: '6f392748-4dd9-11ea-ba35-05698b78935d'
+};
+
+async function runForOrder(
+  order: models.Order,
+  type: string,
+  requestBuilder: RequestBuilder,
+) {
+  const request = await requestBuilder.createOrder(order);
+  console.log(`\n*** ${type} Order ***`);
+  console.log(request);
+  console.log(
+    RequestVerifier.createOrder(request)
+      ? 'Signature verified ✅'
+      : 'Signature invalid ❌',
+  );
+}
+
 async function run() {
   const wallet = new ethers.Wallet(
     '0x3141592653589793238462643383279502884197169399375105820974944592',
   );
   const requestBuilder = new RequestBuilder(wallet);
 
-  const limitOrder: models.LimitOrder = {
-    market: 'IDEX-ETH',
-    side: 'buy',
-    type: 'limit',
-    quantity: '1.20000000',
-    price: '0.50000000',
-    // customClientOrderId: '6f392746-4dd9-11ea-ba35-05698b78935d'
-  };
-  const limitRequest = await requestBuilder.createOrder(limitOrder);
-  console.log('\n*** Limit Order ***');
-  console.log(limitRequest);
-  console.log(
-    RequestVerifier.createOrder(limitRequest)
-      ? 'Signature verified ✅'
-      : 'Signature invalid ❌',
-  );
-
-  const marketOrder: models.MarketOrder = {
-    market: 'IDEX-ETH',
-    side: 'sell',
-    type: 'market',
-    quantity: '1.20000000',
-    // customClientOrderId: '6f392747-4dd9-11ea-ba35-05698b78935d'
-  };
-  const marketRequest = await requestBuilder.createOrder(marketOrder);
-  console.log('\n*** Market Order ***');
-  console.log(marketRequest);
-  console.log(
-    RequestVerifier.createOrder(marketRequest)
-      ? 'Signature verified ✅'
-      : 'Signature invalid ❌',
-  );
-
-  const stopLimitOrder: models.StopLimitOrder = {
-    market: 'IDEX-ETH',
-    side: 'buy',
-    type: 'stopLimit',
-    quantity: '1.20000000',
-    price: '0.50000000',
-    stopPrice: '0.60000000',
-    // customClientOrderId: '6f392748-4dd9-11ea-ba35-05698b78935d'
-  };
-  const stopLimitRequest = await requestBuilder.createOrder(stopLimitOrder);
-  console.log('\n*** Market Order ***');
-  console.log(stopLimitRequest);
-  console.log(
-    RequestVerifier.createOrder(stopLimitRequest)
-      ? 'Signature verified ✅'
-      : 'Signature invalid ❌',
-  );
+  await runForOrder(limitOrder, 'Limit', requestBuilder);
+  await runForOrder(marketOrder, 'Market', requestBuilder);
+  await runForOrder(stopLimitOrder, 'Stop Limit', requestBuilder);
 }
 
 run();
