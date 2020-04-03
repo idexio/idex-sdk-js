98ce8d4d11ed439d844a5b466ffd77e267a0fd0b
dileepfrog <dileepfrog@gmail.com>
2020-04-03 14:05:52 -0700

    add eslint disables for export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import AuthenticatedClient from './clients/authenticated';
 import PublicClient from './clients/public';
+// TODO eslint doesn't yet understand the syntax export * as ns
+/* eslint-disable-next-line */
 import { enums, request, response } from './types';
 
 export { AuthenticatedClient, PublicClient, enums, request, response };
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable */
+// TODO eslint doesn't yet understand the syntax export * as ns
 export * as enums from './enums';
 export * as request from './request';
 export * as response from './response';
