e7810b635e55ab47603f20724650e75999695175
alexhustler <alexhustler@users.noreply.github.com>
2020-04-16 21:12:42 +0000

    Export webSocket types (#11)
    * switch to camelcase webSocket

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import AuthenticatedClient from './clients/authenticated';
 import PublicClient from './clients/public';
-import WebsocketClient from './clients/websocket';
+import WebSocketClient from './clients/webSocket';
 // TODO eslint doesn't yet understand the syntax export * as ns
 /* eslint-disable-next-line */
-import { enums, request, response } from './types';
+import { enums, request, response, webSocket } from './types';
 import { getPrivateKeySigner } from './ethers/sign';
 
 export {
   AuthenticatedClient,
   PublicClient,
-  WebsocketClient,
+  WebSocketClient,
   enums,
   request,
   response,
+  webSocket,
   getPrivateKeySigner,
 };
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,4 +3,4 @@
 export * as enums from './enums';
 export * as request from './request';
 export * as response from './response';
-export * as websocket from './websocket';
+export * as webSocket from './webSocket';
