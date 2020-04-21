aaaea8be84a2d7c8e809f244b6b103a1c3f6510b
dileepfrog <dileepfrog@gmail.com>
2020-04-20 23:16:59 -0700

    fix lint issues

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import AuthenticatedClient from './clients/authenticated';
 import PublicClient from './clients/public';
 import WebSocketClient from './clients/webSocket';
-// TODO eslint doesn't yet understand the syntax export * as ns
-/* eslint-disable-next-line */
 import { enums, request, response, webSocket } from './types';
 import { getPrivateKeySigner } from './ethers';
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
-/* eslint-disable */
-// TODO eslint doesn't yet understand the syntax export * as ns
-export * as enums from './enums';
-export * as request from './request';
-export * as response from './response';
-export * as webSocket from './webSocket';
+import * as enums from './enums';
+import * as request from './request';
+import * as response from './response';
+import * as webSocket from './webSocket';
+
+export { enums, request, response, webSocket };
