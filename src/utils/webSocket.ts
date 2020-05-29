bb677725000525e50faeaa2d79d1dbfff2f2c6cf
alexhustler <alexhustler@users.noreply.github.com>
2020-05-29 19:44:11 -0400

    p9-1347 add wsToken response (#32)
    - update ticker and candle response types

--- src/utils/webSocket.ts
+++ src/utils/webSocket.ts
@@ -13,15 +13,14 @@ const transformTickersMessage = (
   high: ticker.h,
   low: ticker.l,
   close: ticker.c,
-  last: ticker.c,
-  lastQuantity: ticker.Q,
+  closeQuantity: ticker.Q,
   baseVolume: ticker.v,
   quoteVolume: ticker.q,
   percentChange: ticker.P,
   ...(ticker.n && { numTrades: ticker.n }),
   ask: ticker.a,
   bid: ticker.b,
-  lastSequenceNumber: ticker.u,
+  sequence: ticker.u,
 });
 
 const transformTradesMessage = (
@@ -43,8 +42,8 @@ const transformCandlesMessage = (
   market: candle.m,
   time: candle.t,
   interval: candle.i,
-  startTime: candle.s,
-  endTime: candle.e,
+  start: candle.s,
+  end: candle.e,
   open: candle.o,
   high: candle.h,
   low: candle.l,
