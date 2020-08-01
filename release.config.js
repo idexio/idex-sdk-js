3c810820b06bae261e92ade2ed4ba752feb2f9ae
Braden Napier <brady.napier@dashos.net>
2020-08-01 01:18:07 -0400

    ci(actions): test workflow_dispatch action

--- release.config.js
+++ release.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
+    'next',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true },
+  ],
   plugins: [
     [
       '@semantic-release/commit-analyzer',
