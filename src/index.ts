acc28b0e41e497cba208efa4aee30d199ee04b26
Braden Napier <brady.napier@dashos.net>
2020-08-06 06:12:32 -0400

    chore: provide legacy interface for no breaking changes

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ export * from './types';
 export * from './client';
 export * from './constants';
 export * from './signatures';
+// legacy exports - remove once migrated
+export * as signatures from './signatures';
+export * as types from './types';
+export * as client from './client';
+export * as constant from './constants';
